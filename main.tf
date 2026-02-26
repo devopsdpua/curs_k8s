947c63db134a060af104a892735718b6101a87a9
Ruslan Solovey <Solovei1989>
2026-02-27 00:32:34 +0200

    update

--- main.tf
+++ main.tf
@@ -141,7 +141,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 resource "azurerm_kubernetes_cluster_node_pool" "workload" {
   name                  = "workload"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = "Standard_B2ps_v2"
+  vm_size               = "Standard_D2as_v4"
 
   auto_scaling_enabled = true
   min_count            = 1
